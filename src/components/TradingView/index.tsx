ad7d33416e97692670e4df60b856abd0fcc22fe0
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2021-12-22 21:40:39 +0100

    feat: prediction line

--- src/components/TradingView/index.tsx
+++ src/components/TradingView/index.tsx
@@ -8,7 +8,6 @@ import {
 import { useMarket } from '../../utils/markets';
 import * as saveLoadAdapter from './saveLoadAdapter';
 import { flatten } from '../../utils/utils';
-import Datafeed from './api/';
 import scalping from './api/scalping';
 import intra from './api/intra';
 import swing from './api/swing';
@@ -17,11 +16,7 @@ export interface ChartContainerProps {
   symbol: ChartingLibraryWidgetOptions['symbol'];
   interval: ChartingLibraryWidgetOptions['interval'];
   auto_save_delay: ChartingLibraryWidgetOptions['auto_save_delay'];
-  // BEWARE: no trailing slash is expected in feed URL
-  // datafeed: any;
-  datafeedUrl: string;
   libraryPath: ChartingLibraryWidgetOptions['library_path'];
-  chartsStorageUrl: ChartingLibraryWidgetOptions['charts_storage_url'];
   chartsStorageApiVersion: ChartingLibraryWidgetOptions['charts_storage_api_version'];
   clientId: ChartingLibraryWidgetOptions['client_id'];
   userId: ChartingLibraryWidgetOptions['user_id'];
@@ -31,55 +26,45 @@ export interface ChartContainerProps {
   containerId: ChartingLibraryWidgetOptions['container_id'];
   theme: string;
 }
-
 export interface ChartContainerState {}
 
-export const TVChartContainer = (props) => {
-  // let datafeed = useTvDataFeed();
-  let Data = Datafeed;
+const options = {
+  scalping: {
+    data: scalping,
+    defaultInterval: '5',
+  },
+  intra: {
+    data: intra,
+    defaultInterval: '60',
+  },
+  swing: {
+    data: swing,
+    defaultInterval: '240',
+  },
+  position: {
+    data: position,
+    defaultInterval: '1W',
+  },
+};
 
+export const TVChartContainer = (props) => {
   let defaultProps: ChartContainerProps = {
     symbol: 'Binance:BTC/USD',
-    // @ts-ignore
-    interval: '15',
+    interval: options[props.info.modality].defaultInterval,
     auto_save_delay: 5,
     theme: 'Dark',
     containerId: 'tv_chart_container',
-    //datafeed: Datafeed,
     libraryPath: '/charting_library/',
     chartsStorageApiVersion: '1.1',
     clientId: 'tradingview.com',
     userId: 'public_user_id',
     fullscreen: false,
     autosize: true,
-    //datafeedUrl: BONFIDA_DATA_FEED,
     studiesOverrides: {},
   };
 
-  if (props.info.modality == 'scalping') {
-    Data = scalping;
-    // @ts-ignore
-    defaultProps.interval = '5';
-  }
-  if (props.info.modality == 'intra') {
-    Data = intra;
-    // @ts-ignore
-    defaultProps.interval = '60';
-  }
-  if (props.info.modality == 'swing') {
-    Data = swing;
-    // @ts-ignore
-    defaultProps.interval = '240';
-  }
-  if (props.info.modality == 'position') {
-    Data = position;
-    // @ts-ignore
-    defaultProps.interval = '1W';
-  }
-
   const tvWidgetRef = React.useRef<IChartingLibraryWidget | null>(null);
   const { market } = useMarket();
-
   const chartProperties = JSON.parse(
     localStorage.getItem('chartproperties') || '{}',
   );
@@ -88,18 +73,13 @@ export const TVChartContainer = (props) => {
     const savedProperties = flatten(chartProperties, {
       restrictTo: ['scalesProperties', 'paneProperties', 'tradingProperties'],
     });
-
     const widgetOptions: ChartingLibraryWidgetOptions = {
       symbol: props.info.symbol,
-      // BEWARE: no trailing slash is expected in feed URL
-      // tslint:disable-next-line:no-any
-      // @ts-ignore
-      datafeed: Data,
+      datafeed: options[props.info.modality].data,
       interval: defaultProps.interval as ChartingLibraryWidgetOptions['interval'],
       container_id: defaultProps.containerId as ChartingLibraryWidgetOptions['container_id'],
       library_path: defaultProps.libraryPath as string,
       auto_save_delay: 5,
-
       locale: 'en',
       disabled_features: [
         'use_localstorage_for_settings',
@@ -156,44 +136,85 @@ export const TVChartContainer = (props) => {
         },
       },
     };
-
     const tvWidget = new widget(widgetOptions);
 
     tvWidget.onChartReady(() => {
       tvWidgetRef.current = tvWidget;
-      tvWidget
-        // @ts-ignore
-        .subscribe('onAutoSaveNeeded', () => tvWidget.saveChartToServer());
-
-      console.log(tvWidget.chart());
-      console.log(widgetOptions);
-
-       // @ts-ignore
-      const bars = tvWidget.chart().getSeries().data().m_bars;
-      const lastBar = bars._items[bars._end-1];
-      const lastBarData = {
-        time: lastBar.exTime,
-        open: lastBar.value[1],
-        close: lastBar.value[4],
-        percent: (lastBar.value[1] / 100) * 10000
-      }
-      console.log(bars);
-      console.log(lastBarData);
-      console.log(
-        tvWidget
-          .chart()
-          .createShape(
-            { time: lastBarData.time, channel: 'close' },
-            {
-              shape: 'long_position',
-              lock: true,
-              overrides: {
-                profitLevel: lastBarData.percent * 3,
-                stopLevel: lastBarData.percent * 1.5,
-              },
-            },
-          ),
+      tvWidget.subscribe('onAutoSaveNeeded', () =>
+        tvWidget.saveChartToServer(),
       );
+      console.log(tvWidget.chart());
+
+      const priceScale = 10000;
+      const position = {
+        direction: 'long_position',
+        takeProfit: 0.03,
+        stopLoss: 0.015,
+        bars: 10,
+      };
+
+      // get last bar information
+      // @ts-ignore
+      const bars = tvWidget.chart().getSeries().data().m_bars,
+        lastBar = bars._items[bars._end - 1],
+        lastBarData = {
+          time: lastBar.exTime,
+          open: lastBar.value[1],
+          close: lastBar.value[4],
+          percent: lastBar.value[1] * priceScale,
+          distance: lastBar.exTime - bars._items[bars._end - 2].exTime,
+        };
+
+      // position shape
+      tvWidget.chart().createMultipointShape(
+        [
+          { time: lastBarData.time, price: lastBarData.close },
+          {
+            time: lastBarData.time + lastBarData.distance * position.bars,
+            price: lastBarData.close,
+          },
+        ],
+        {
+          // @ts-ignore
+          shape: position.direction,
+          lock: true,
+          overrides: {
+            profitLevel: lastBarData.percent * position.takeProfit,
+            stopLevel: lastBarData.percent * position.stopLoss,
+          },
+        },
+      );
+
+      // prediction line
+      tvWidget
+        .chart()
+        .createOrderLine()
+        .onMove(function () {
+          // @ts-ignore
+          console.log(this);
+          // @ts-ignore
+          const actualPrice = this.getPrice();
+          const takeProfitPrice = lastBarData.close * (1 + position.takeProfit);
+          const stopLossPrice = lastBarData.close * (1 - position.stopLoss);
+          let newPrice = actualPrice;
+
+          if (actualPrice > takeProfitPrice) newPrice = takeProfitPrice;
+          else if (actualPrice < stopLossPrice) newPrice = stopLossPrice;
+
+          const takeProfitProximity =
+            (newPrice >= lastBarData.close
+              ? (newPrice - lastBarData.close) /
+                (takeProfitPrice - lastBarData.close)
+              : (lastBarData.close - newPrice) /
+                (stopLossPrice - lastBarData.close)) * 100;
+
+          // @ts-ignore
+          if (newPrice !== actualPrice) this.setPrice(newPrice);
+          // @ts-ignore
+          this.setQuantity(Math.round(takeProfitProximity) + '%');
+        })
+        .setText('Prediction')
+        .setQuantity('0%');
     });
   }, [market, tvWidgetRef.current]);
 
